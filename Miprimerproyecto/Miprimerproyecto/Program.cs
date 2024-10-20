cc42e9312c5b59aea241f16dadb96928c6495b83
Ricardo Castro <ricar@DESKTOP-7ADB7C0>
2024-10-20 14:35:52 -0600

    Creaccion de mi primer proyecto

---
+++ Miprimerproyecto/Miprimerproyecto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miprimerproyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hola chicos de programacion computacional I, Bienvenidos");
+
+            Console.ReadLine();//Para que no se cierre la consola
+        }
+    }
+}
