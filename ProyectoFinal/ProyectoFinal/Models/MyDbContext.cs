5592b674c5b92dcce346d69c73671f0cdbe0b844
Ricardo Castro <ricar@DESKTOP-7ADB7C0>
2024-11-10 15:26:28 -0600

    proyecto final

---
+++ ProyectoFinal/ProyectoFinal/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoFinal.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Pelicula> Peliculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Pelicula>().HasKey(pelicula => pelicula.IdPeliculas);
+
+        }
+    }
+}
---
+++ ProyectoFinal/ProyectoFinal/Models/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Models
+{
+    public class Pelicula
+    {
+        [Key]
+        public int IdPeliculas { get; set; }
+        public string Titulo { get; set; }
+        public string Autor {  get; set; }
+        public string Sinopsis { get; set; }
+        public string Duracion { get; set; }
+        public string Clasificacion { get; set;}
+        public byte[] Foto { get; set; }
+    }
+}
